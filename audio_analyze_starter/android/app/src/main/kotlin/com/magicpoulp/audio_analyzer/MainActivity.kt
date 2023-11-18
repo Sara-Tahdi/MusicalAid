00be1fa0e204518dadbef509fb165f503716c5ab
connortate <connor.tate@mail.mcgill.ca>
2023-11-18 16:49:03 -0500

    Deleted garbage

--- audio_analyze_starter/android/app/src/main/kotlin/com/magicpoulp/audio_analyzer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.magicpoulp.audio_analyzer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- gestures/android/app/src/main/kotlin/com/example/gestures/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gestures
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
